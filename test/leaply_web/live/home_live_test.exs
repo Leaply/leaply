bbd9ff75bb2f52f9163d87a1c147259ed32b41f4
Exa Stencil <git@exastencil.com>
2020-08-28 09:34:09 +0200

    Fix homepage test

--- test/leaply_web/live/home_live_test.exs
+++ test/leaply_web/live/home_live_test.exs
@@ -1,11 +1,11 @@
-defmodule LeaplyWeb.PageLiveTest do
+defmodule LeaplyWeb.HomeLiveTest do
   use LeaplyWeb.ConnCase
 
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Before they sold out"
+    assert render(page_live) =~ "Before they sold out"
   end
 end
